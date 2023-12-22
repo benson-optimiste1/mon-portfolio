b7307204bd54377730ee9bd2acd78bd8c6d42c15
benson-optimiste1 <chatelainevelybenson@gmail.com>
2023-12-22 13:47:51 -0500

    My_PORTFOLIO

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header>
+      <h1>My Portfolio</h1>
+      {/* Add any additional content or components here */}
+    </header>
+  );
+};
+
+export default Header;
