778ac73a434db4f62d2643e5876a324f2045615e
benson-optimiste1 <chatelainevelybenson@gmail.com>
2023-12-22 14:00:08 -0500

    component added

---
+++ src/components/AboutMe.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const AboutMe = () => {
+    return (
+        <div>
+            <h1>About Me</h1>
+            <p>Write your about me content here.</p>
+        </div>
+    );
+};
+
+export default AboutMe;
---
+++ src/components/Projects.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Projects = () => {
+  return (
+    <div>
+      {/* Your project content goes here */}
+    </div>
+  );
+};
+
+export default Projects;
