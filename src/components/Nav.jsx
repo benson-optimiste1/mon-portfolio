66985a7d644b524cd2d118daf0f5513aa04098f5
benson-optimiste1 <chatelainevelybenson@gmail.com>
2024-01-02 16:14:44 -0500

    something add

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -90,11 +90,11 @@ export default function Nav() {
                     <Menu.Button className="relative flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
                       <span className="absolute -inset-1.5" />
                       <span className="sr-only">Open user menu</span>
-                      <img
+                      {/* <img
                         className="h-8 w-8 rounded-full"
                         src="./public/image/profile.jpg"
                         alt="profile"
-                      />
+                      /> */}
                     </Menu.Button>
                   </div>
                   <Transition
@@ -106,7 +106,7 @@ export default function Nav() {
                     leaveFrom="transform opacity-100 scale-100"
                     leaveTo="transform opacity-0 scale-95"
                   >
-                    <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                    {/* <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
                       <Menu.Item>
                         {({ active }) => (
                           <a
@@ -138,6 +138,7 @@ export default function Nav() {
                         )}
                       </Menu.Item>
                     </Menu.Items>
+                  </Menu> */}
                   </Transition>
                 </Menu>
               </div>
