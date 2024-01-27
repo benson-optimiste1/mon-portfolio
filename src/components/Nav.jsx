481993b1572d8df1c9dfcbae5af0c00b12a244a7
benson-optimiste1 <chatelainevelybenson@gmail.com>
2024-01-26 23:26:10 -0500

    something changeg

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -71,7 +71,7 @@ export default function Nav() {
                 {/* Profile dropdown */}
                 <Menu as="div" className="relative ml-3">
                   <div>
-                    <Menu.Button className="relative flex text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
+                    
                       <span className="absolute -inset-1.5" />
                       <span className="sr-only">Open user menu</span>
                       <img
@@ -79,7 +79,7 @@ export default function Nav() {
                         src="/images/logo.png"
                         alt=""
                       />
-                    </Menu.Button>
+                   
                   </div>
                   <Transition
                     as={Fragment}
